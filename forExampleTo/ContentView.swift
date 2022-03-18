b2114ebe65b8f1ca72807b3db2e885e7d11b7fe9
VA <VA>
2022-03-18 17:31:12 +0500

    Initial Commit

---
+++ forExampleTo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  forExampleTo
+//
+//  Created by Владимир Осетров on 18.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ forExampleTo/forExampleToApp.swift
@@ -0,0 +1,17 @@
+//
+//  forExampleToApp.swift
+//  forExampleTo
+//
+//  Created by Владимир Осетров on 18.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct forExampleToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
