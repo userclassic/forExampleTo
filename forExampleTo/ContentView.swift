cd35e63849d449cf4bb488c9f3e798b73f365125
VA <VA>
2022-03-18 17:36:36 +0500

    add

--- forExampleTo/ContentView.swift
+++ forExampleTo/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello,my world!")
+                .padding()
+                .font(.largeTitle.bold())
+            Text ("good day = Friday")
+                .padding()
+                .font(.headline.monospaced().weight(.ultraLight))
+        }
     }
 }
 
